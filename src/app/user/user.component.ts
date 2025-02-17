5d79e57ef535549bfffd3552cf1e240a1f263405
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-17 11:29:37 +0500

    output and eventemitter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, input, computed } from '@angular/core';
+import {
+  Component,
+  Input,
+  input,
+  computed,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,14 +14,22 @@ import { Component, Input, input, computed } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  iamgePath = computed(() => {
-    return 'assets/Users/' + this.avatar();
-  });
-  onSelectUser() {}
+  // iamgePath = computed(() => {
+  //   return 'assets/Users/' + this.avatar();
+  // });
+  iamgePath() {
+    return 'assets/Users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
