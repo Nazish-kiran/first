d9f89f8b367b3392b3a7ed83f7313626afc4fb4c
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-17 13:31:24 +0500

    added task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import{UserComponent} from "./user/user.component";
+import { TaskComponent } from './task/task.component';
 import { DUMMY_USERS } from './dummyUser';
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent , UserComponent],
+  imports: [ HeaderComponent , UserComponent , TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first';
   users = DUMMY_USERS;
+  onSelectUser(id: string){
+console.log('user has an id :' + id);
+
+  }
+
 }
