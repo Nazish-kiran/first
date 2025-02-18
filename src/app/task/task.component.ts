c8c4f92fd9a73e20d70c4decd9b3bb8e02ec0d3d
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-18 16:36:48 +0500

    added dummy data

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { UserTaskComponent } from './user-task/user-task.component';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [UserTaskComponent],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-@Input() names: string | undefined;
+  @Input() names: string | undefined;
+
+  task = [{
+    id:'t1',
+    userId:'u1',
+    title:'Master Angular',
+    summary:'Learn all the basic and advance features of angular and how to use them',
+    dueDate:'2025-12-31'
+  }];
 }
